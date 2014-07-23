e8656e7b98b1e7f550655e7fbbb8e064520bb451
dturner <dturner@kixeye.com>
2014-07-23 13:51:10 -0700

    added @Init and @Destroy examples

--- src/main/java/com/kixeye/template/TemplateConfiguration.java
+++ src/main/java/com/kixeye/template/TemplateConfiguration.java
@@ -1,23 +1,15 @@
 package com.kixeye.template;
 
-import com.kixeye.chassis.bootstrap.BootstrapException.ApplicationConfigurationNotFoundException;
 import com.kixeye.chassis.bootstrap.annotation.App;
-import com.kixeye.chassis.bootstrap.annotation.SpringApp;
-import com.kixeye.chassis.bootstrap.aws.ServerInstanceContext;
+import com.kixeye.chassis.bootstrap.annotation.Destroy;
+import com.kixeye.chassis.bootstrap.annotation.Init;
 import com.kixeye.chassis.bootstrap.configuration.ConfigurationProvider;
-import com.kixeye.chassis.bootstrap.configuration.zookeeper.ZookeeperConfigurationProvider;
 import com.kixeye.chassis.support.ChassisConfiguration;
 import com.kixeye.chassis.transport.TransportConfiguration;
-import com.netflix.config.ConcurrentMapConfiguration;
-import org.apache.commons.configuration.AbstractConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.IOException;
 
 /**
  * A template configuration.
@@ -35,15 +27,32 @@ import java.io.IOException;
 @ComponentScan(basePackageClasses = TemplateConfiguration.class)
 public class TemplateConfiguration {
 
-    @PostConstruct
-    public void init() {
-        System.out.println();
-        // Add some initialization logic here!!
+    /*
+     * use this method to perform any initialization required by the application prior to application startup. You
+     * can optionally get a reference to the ConfigurationProvider
+     */
+    @Init
+    public static void init(org.apache.commons.configuration.Configuration configuration/*, com.kixeye.chassis.bootstrap.configuration.ConfigurationProvider*/) {
     }
 
+    /*
+     * use this method to perform any teardown required after the application is stopped
+     */
+    @Destroy
+    public static void destroy() {
+    }
+
+    /*
+     * use this method to perform any tasks that need to be done after initial application startup
+     */
+    @PostConstruct
+    public void postConstruct() {
+    }
+
+    /*
+     * use this method to perform any tasks taht need to be done just before the application stops
+     */
     @PreDestroy
-    public void destroy() {
-        System.out.println();
-        // Add some destruction code here!
+    public void preDestroy() {
     }
 }
